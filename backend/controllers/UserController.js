2ca7172a92fdc7f82fc07367227f86cf809436ad
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-20 17:01:12 -0300

    commit get user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,8 +1,11 @@
 const User = require('../models/User')
 
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+// helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -40,13 +43,13 @@ module.exports = class UserController {
         // check if user exists
         const userExistis = await User.findOne({ email: email })
 
-        if(userExistis){
+        if (userExistis) {
             res.status(422).json({
                 message: 'Por favor, utilize outro e-mail!'
             })
             return
-        }  
-        
+        }
+
         // create a password 
         const salt = await bcrypt.genSalt(12)
         const passwordHash = await bcrypt.hash(password, salt)
@@ -64,7 +67,76 @@ module.exports = class UserController {
             await createUserToken(newUser, req, res)
             return
         } catch (error) {
-            res.status(500).json({message: error})
+            res.status(500).json({ message: error })
         }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body
+
+        if (!email) {
+            res.status(422).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+        // check if user exists
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            res.status(422).json({
+                message: 'Não há usuário cadastrado com este e-mail!'
+            })
+            return
+        }
+
+        // check if password match with db password
+
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if (!checkPassword) {
+            res.status(422).json({
+                message: 'Senha inválida!'
+            })
+            return
+        }
+
+        await createUserToken(user, req, res)
+
+    }
+
+    static async checkUser(req, res) {
+        let currentUser
+
+        if (req.headers.authorization) {
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+        } else {
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res) {
+
+        const id = req.params.id
+        const user = await User.findById(id).select('-password')
+
+        if (!user) {
+            res.status(422).json({
+                message: 'Usuário não econtrado!'
+            })
+            return
+        }
+
+        res.status(200).json({ user })
     }
 }
