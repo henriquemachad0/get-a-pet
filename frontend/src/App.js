e7b54725cf997b5a460c194232a10037ee90684c
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-22 19:12:46 -0300

    commit register e logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Container from './components/layout/Container'
+import Message from './components/layout/Message'
 
 /* pages */
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 
+/* context */
+import { UserProvider } from './context/UserContext'
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />}>
-          </Route>
+      <UserProvider>
+        <Navbar />
+        <Message />
+        <Container>
+          <Routes>
+            
+            <Route path="/login" element={<Login />}>
+            </Route>
 
-          <Route path="/register" element={<Register />}>
-          </Route>
+            <Route path="/register" element={<Register />}>
+            </Route>
 
-          <Route path="/" element={<Home />}>
-          </Route>
-        </Routes>
-      </Container>
-      <Footer />
+            <Route path="/" element={<Home />}>
+            </Route>
+
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,26 +1,42 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
 
 import styles from './Navbar.module.css'
 
 import Logo from '../../assets/img/logo.png'
 
+/* context */
+import { Context } from '../../context/UserContext'
+
 function Navbar() {
+
+    const { authenticated, logout } = useContext(Context)
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar_logo}>
-            <img src={Logo} alt="Get a Pet" />
-            <h2>Get a Pet</h2>
+                <img src={Logo} alt="Get a Pet" />
+                <h2>Get a Pet</h2>
             </div>
             <ul>
                 <li>
                     <Link to="/">Adotar</Link>
                 </li>
-                <li>
-                    <Link to="/login">Entrar</Link>
-                </li>
-                <li>
-                    <Link to="/register">Cadastrar</Link>
-                </li>
+                {
+                    authenticated ? (
+                        <>
+                            <li onClick={logout}>Sair</li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to="/login">Entrar</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Cadastrar</Link>
+                            </li>
+                        </>
+                    )}
             </ul>
         </nav>
     )
